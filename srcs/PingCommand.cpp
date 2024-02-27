c04e1a57bab139cd26cfc525edcd28f5a3b17860
okbrandon <okbrandonlooksdumb@gmail.com>
2024-02-27 13:51:17 +0100

    :bug: Fixed PING command response

--- srcs/PingCommand.cpp
+++ srcs/PingCommand.cpp
@@ -6,7 +6,7 @@
 /*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 17:20:06 by bsoubaig          #+#    #+#             */
-/*   Updated: 2024/02/23 11:59:09 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2024/02/27 13:48:52 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,8 @@ void	PingCommand::execute(void) const {
 	if (this->_args.size() < 1)
 		throw ERR_NEEDMOREPARAMS(this->_name);
 
-	this->_user->addSendBuffer("PONG :" + this->_user->getNickname() + "\r\n");
+	std::string	userId = USER_IDENTIFIER(this->_user->getNickname(), this->_user->getUsername());
+	std::string	response = userId + " PONG :" + this->_user->getNickname() + "\r\n";
+
+	this->_user->addSendBuffer(response);
 }
